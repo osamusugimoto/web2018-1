ec71393a31845f5ba8c819d32cf887985ee4748b
sam <sam@josai.ac.jp>
2018-10-07 20:40:44 -0400

    10082018

---
+++ ex06-5.js
@@ -0,0 +1,16 @@
+
+var who65 = 'world';
+// var come = 'が来た'
+function sayhello65() {
+// var who = 'world';
+  alert('Hello, ' + who65 + '!');
+} 
+
+function taro() {
+  who65 = '太郎' ;
+} 
+function hanako() {
+  who65 = '花子';
+}
+function gakita() {alert(who65 + 'が来た');
+}
