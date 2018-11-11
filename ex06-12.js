695c1b9fffc7d7da22f7c1cebbfe467fd72d3469
sam <sam@josai.ac.jp>
2018-11-11 11:23:09 -0500

    11122018

--- ex06-12.js
+++ ex06-12.js
@@ -1,9 +1,19 @@
 
 function ThreeSecChange() { 
 //関数imgchange1()を3000ミリ秒間隔で呼び出す 
-  setInterval("imgchange1()",3000); } 
+  setInterval("imgchange12()",3000); } 
   
 
-function imgchange1() {
+function imgchange12() {
   document.getElementById('logo2').src = './image/bulbon.gif';
 }
+
+
+//      <input type="button" value="画像の変更" onclick="imgchange1();">
+//  <img src='./image/josai.png' id="logo" alt="Josai Logo">
+
+// * ex06-9.js
+// function imgchange1() {
+//  document.getElementById('logo').src = './image/bulbon.gif';
+
+
