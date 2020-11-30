b279669bf4b454754e8c81aeb8d98b38647c8538
Osamu Sugimoto <sam@josai.ac.jp>
2020-11-30 14:12:59 +0000

    11302020

--- ex06-9.js
+++ ex06-9.js
@@ -5,3 +5,17 @@ function txtchange1() {
 function imgchange1() {
   document.getElementById('logo').src = './image/bulbon.gif';
 }
+function show_innerHTMLtxt1(){
+	var result1 = document.getElementById('txt1').innerHTML;
+    alert(result1);
+}
+
+function show_innerHTML(){
+	var result = document.getElementById('logo').innerHTML;
+    alert(result);
+}
+
+// do not work
+// function imgchange2() {
+//   document.getElementById('logo').innerHTML = './image/bulbon.gif';
+// }
